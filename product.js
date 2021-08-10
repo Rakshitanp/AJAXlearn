1df377984574006fe1a2b8281fe9c9280251819c
Rakshitanp <55306057+Rakshitanp@users.noreply.github.com>
2021-08-10 16:03:48 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,43 @@
+
+var products=[
+	{
+	"name":"Lenovo ThinkPad",
+	"desc":"Laptop",
+	"price":1,50,000
+	},
+	{
+	"name":"Sony Headphone",
+	"desc":"Blue Bluetooth HeadPhone",
+	"price":3,000
+	},
+	{
+	"name":Oneplus 7t,
+	"desc":"Android Phone",
+	"price":30,000
+	},
+	{
+	"name":"Sony Smart TV",
+	"desc":"LED TV",
+	"price":70,000
+	},
+	{
+	"name":"Orange",
+	"desc":"Fruit",
+	"price":200
+	},
+	{
+	"name":"Strawberry",
+	"desc":"Fruit",
+	"price":250
+	},
+	{
+	"name":"Carrot",
+	"desc":"Vegetable",
+	"price":150
+	},
+	{
+	"name":"Paneer",
+	"desc":"Dairy Product",
+	"price":150
+	}
+]
